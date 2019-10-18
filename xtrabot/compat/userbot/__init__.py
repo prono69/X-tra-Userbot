79bc8666ef4e90f9d247ffb21c8e58a3dac596ac
SnapDragon <maliktahir0702@gmail.com>
2019-10-19 01:30:44 +0200

    Update __init__.py

--- xtrabot/compat/userbot/__init__.py
+++ xtrabot/compat/userbot/__init__.py
@@ -1,2 +1,18 @@
+#    X-tra-Telegram (userbot for telegram)
+#    Copyright (C) 2019-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from xtrabot.utils import SupportMods
 BOTLOG_CHATID,BOTLOG,LOGSPAMMER,PM_AUTO_BAN,CONSOLE_LOGGER_VERBOSE,DB_URI,OCR_SPACE_API_KEY,REM_BG_API_KEY,CHROME_DRIVER,GOOGLE_CHROME_BIN,OPEN_WEATHER_MAP_APPID,WEATHER_DEFCITY,ANTI_SPAMBOT,ANTI_SPAMBOT_SHOUT,YOUTUBE_API_KEY,ALIVE_NAME,COUNTRY,TZ_NUMBER,CLEAN_WELCOME,BIO_PREFIX,DEFAULT_BIO,LASTFM_API,LASTFM_SECRET,LASTFM_USERNAME,LASTFM_PASSWORD_PLAIN,LASTFM_PASS,lastfm,G_DRIVE_CLIENT_ID ,G_DRIVE_CLIENT_SECRET,G_DRIVE_AUTH_TOKEN_DATA,GDRIVE_FOLDER_ID,TEMP_DOWNLOAD_DIRECTORY,COUNT_MSG,USERS,COUNT_PM,LASTMSG,CMD_HELP,ISAFK,AFKREASON,bot = SupportMods().PPESupport().ppevar()
