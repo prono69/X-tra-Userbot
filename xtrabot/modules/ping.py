47988e95efb7157857755c3adb6075ab8c9b33c4
SnapDragon <maliktahir0702@gmail.com>
2019-10-19 13:21:42 +0200

    Create ping.py

---
+++ xtrabot/modules/ping.py
@@ -0,0 +1,37 @@
+#    X-tra-Telegram (userbot for telegram)
+#    Copyright (C) 2019-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from xtrabot import loader
+from datetime import datetime
+
+def start(eb):
+    eb(Ping)
+
+class Ping(loader.Module):
+    def __init__:
+        self.name = "ping"
+        super().__init__(self)
+        self.addxconfig("PING", "Pong!\n", "Defines Ping Message")
+
+    async def pingcmd(self, event):
+        start = datetime.now()
+        await event.edit(self.xconfig["PING"])
+        end = datetime.now()
+        ms = (end - start).microseconds / 1000
+        await event.edit(self.xconfig["PING"]+ms)
+
+    async def aboutpingcmd(self, event):
+        await event.edit(self.xconfig["PING"].about())
