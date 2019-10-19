42d6023c744e0613a89143ade4e56ccf915db277
SnapDragon <maliktahir0702@gmail.com>
2019-10-19 14:01:26 +0200

    Update xtrautil.py

--- xtrabot/xtrautil.py
+++ xtrabot/xtrautil.py
@@ -21,17 +21,8 @@ import sys
 import importlib
 from pathlib import Path
 
-if 1==1:
-    path = Path(f"xtrabot/compat/userbot")
-    name = "xtrabot.compat.userbot"
-    spec = importlib.util.spec_from_file_location(name, path)
-    userb = importlib.util.module_from_spec(spec)
-
-if 1==1:
-    path = Path(f"xtrabot/compat/uniborg")
-    name = "xtrabot.compat.uniborg"
-    spec = importlib.util.spec_from_file_location(name, path)
-    unib = importlib.util.module_from_spec(spec)
+import xtrabot.compat.userbot as userb
+import xtrabot.compat.uniborg as unib
 
 def start_module(shortname):
     path = Path(f"xtrabot/modules/{shortname}.py")
@@ -69,28 +60,4 @@ class Module():
         except Excaption as e:
             print(e)
 
-class SupportMods():
-    def uniadmin(self, pattern=None, **args):
-        args["pattern"] = re.compile(uni.COMMAND_HAND_LER + pattern)
-        if "allow sudo" in args:
-            del args["allow_sudo"]
-        if "allow_edited_updates" in args:
-            del args["allow_edited_updates"]
-        return events.NewMessage(**args)
-    class PPESupport():
-        def register(self, **args):
-            for i in args:
-                if i is not "pattern":
-                    del args[i]
-                elif i is not "outgoing":
-                    del args[i]
-                elif i is not "incoming":
-                    del args[i]
-            def decorator(func):
-                client.add_event_handler(func, events.NewMessage(**args))
-                return func
-            return decorator
 
-        def ppevar(self):
-            BOTLOG_CHATID = ppe.BOTLOG_CHATID;BOTLOG = ppe.BOTLOG;LOGSPAMMER = ppe.LOGSPAMMER;PM_AUTO_BAN = ppe.PM_AUTO_BAN;CONSOLE_LOGGER_VERBOSE = ppe.CONSOLE_LOGGER_VERBOSE;DB_URI = ppe.DB_URI;OCR_SPACE_API_KEY = ppe.OCR_SPACE_API_KEY;REM_BG_API_KEY = ppe.REM_BG_API_KEY;CHROME_DRIVER = ppe.CHROME_DRIVER;GOOGLE_CHROME_BIN = ppe.GOOGLE_CHROME_BIN;OPEN_WEATHER_MAP_APPID = ppe.OPEN_WEATHER_MAP_APPID;WEATHER_DEFCITY = ppe.WEATHER_DEFCITY;ANTI_SPAMBOT = ppe.ANTI_SPAMBOT;ANTI_SPAMBOT_SHOUT = ppe.ANTI_SPAMBOT_SHOUT;YOUTUBE_API_KEY = ppe.YOUTUBE_API_KEY;ALIVE_NAME = ppe.ALIVE_NAME;COUNTRY = ppe.COUNTRY;TZ_NUMBER = ppe.TZ_NUMBER;CLEAN_WELCOME = ppe.CLEAN_WELCOME;BIO_PREFIX = ppe.BIO_PREFIX;DEFAULT_BIO = ppe.DEFAULT_BIO;LASTFM_API = ppe.LASTFM_API;LASTFM_SECRET = ppe.LASTFM_SECRET;LASTFM_USERNAME = ppe.LASTFM_USERNAME;LASTFM_PASSWORD_PLAIN = ppe.LASTFM_PASSWORD_PLAIN;LASTFM_PASS = ppe.LASTFM_PASS;lastfm = ppe.lastfm;G_DRIVE_CLIENT_ID = ppe.G_DRIVE_CLIENT_ID;G_DRIVE_CLIENT_SECRET = ppe.G_DRIVE_CLIENT_SECRET;G_DRIVE_AUTH_TOKEN_DATA = ppe.G_DRIVE_AUTH_TOKEN_DATA;GDRIVE_FOLDER_ID = ppe.GDRIVE_FOLDER_ID;TEMP_DOWNLOAD_DIRECTORY = ppe.TEMP_DOWNLOAD_DIRECTORY;COUNT_MSG = ppe.COUNT_MSG;USERS = ppe.USERS;COUNT_PM = ppe.COUNT_PM;LASTMSG = ppe.LASTMSG;CMD_HELP = ppe.CMD_HELP;ISAFK = ppe.ISAFK;AFKREASON = ppe.AFKREASON;bot = ppe.bot
-            return BOTLOG_CHATID,BOTLOG,LOGSPAMMER,PM_AUTO_BAN,CONSOLE_LOGGER_VERBOSE,DB_URI,OCR_SPACE_API_KEY,REM_BG_API_KEY,CHROME_DRIVER,GOOGLE_CHROME_BIN,OPEN_WEATHER_MAP_APPID,WEATHER_DEFCITY,ANTI_SPAMBOT,ANTI_SPAMBOT_SHOUT,YOUTUBE_API_KEY,ALIVE_NAME,COUNTRY,TZ_NUMBER,CLEAN_WELCOME,BIO_PREFIX,DEFAULT_BIO,LASTFM_API,LASTFM_SECRET,LASTFM_USERNAME,LASTFM_PASSWORD_PLAIN,LASTFM_PASS,lastfm,G_DRIVE_CLIENT_ID ,G_DRIVE_CLIENT_SECRET,G_DRIVE_AUTH_TOKEN_DATA,GDRIVE_FOLDER_ID,TEMP_DOWNLOAD_DIRECTORY,COUNT_MSG,USERS,COUNT_PM,LASTMSG,CMD_HELP,ISAFK,AFKREASON,bot
